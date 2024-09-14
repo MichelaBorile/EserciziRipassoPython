a3dd2fe0a04d2964119ab869854bef136f562120
Michela <michela.borile@itiscuneo.eu>
2024-09-14 11:16:57 +0200

    plus comments

--- 01-tabellina.py
+++ 01-tabellina.py
@@ -1,9 +1,13 @@
-
+#tabellina del 4
+#range (inizio, fine, step)
 for i in range(4, 41, 4):
     print(i)
 
+#altri due metodi
 for i in range(11):
     print(4*i)
 
 for i in range(10):
     print(4*(i+1))
+
+
