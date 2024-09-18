42a6fac3490889af415e84c605a9997dfdae8c83
Michela <michela.borile@itiscuneo.eu>
2024-09-18 18:58:50 +0200

    Risoluzione

--- Es-01.py
+++ Es-01.py
@@ -1,1 +1,7 @@
 #1) Da lista1 = [1, 2, 3, 4, 5], creo lista2 che ha tutti i valori raddoppiati
+lista1 =[1, 2, 3, 4, 5]
+
+for i in lista1:
+    lista2 =[number *2 for number in lista1]
+
+print(lista2)
