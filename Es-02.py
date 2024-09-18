172a6f1bc9746dd2d37bd47b5c49697d99677270
Michela <michela.borile@itiscuneo.eu>
2024-09-18 19:05:48 +0200

    risoluzine es2

--- Es-02.py
+++ Es-02.py
@@ -1,1 +1,9 @@
 #2) Trovo il fattoriale di un numero inserito in input
+def fattoriale(n):
+    if n==0 or n== 1:
+        return 1
+    else:
+        return n * fattoriale(n-1)
+number = int(input("Inserisci un numero: "))
+
+print(fattoriale(number))
