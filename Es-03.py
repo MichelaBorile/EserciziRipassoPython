38e68d94e93d0fa1de998479e19943b7b92cf9cb
Michela <michela.borile@itiscuneo.eu>
2024-09-18 19:21:06 +0200

    tentativo, non funziona

--- Es-03.py
+++ Es-03.py
@@ -1,1 +1,9 @@
 #3) Dizionario: {"a": 1, "b": 2, "c": 3}, scambio la posizione tra chiave e valore
+dictionary = {"a": 1, "b": 2, "c": 3}
+
+for key, value in enumerate(dictionary):
+    t = key
+    key = value
+    value = t
+
+print(dictionary)
