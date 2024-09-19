180d0368444c786fe1de1af4a3978b4c6ec51f34
Michela <michela.borile@itiscuneo.eu>
2024-09-19 18:38:40 +0200

    funzionante

--- Es-03.py
+++ Es-03.py
@@ -1,9 +1,7 @@
 #3) Dizionario: {"a": 1, "b": 2, "c": 3}, scambio la posizione tra chiave e valore
 dictionary = {"a": 1, "b": 2, "c": 3}
+dictionary_reverse ={}
+for i in dictionary:
+    dictionary_reverse.setdefault(dictionary[i], i)
 
-for key, value in enumerate(dictionary):
-    t = key
-    key = value
-    value = t
-
-print(dictionary)
+print(dictionary_reverse)
